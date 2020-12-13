de5886754724c484393bff7a349516abb8a75295
Pavel Chernyshev <titan100695@yandex.ru>
2020-12-13 22:28:14 +0300

    map view controller preparing

---
+++ MyPlaces/MyPlaces/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Павел Чернышев on 13.12.2020.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closeMap(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
